1491fdbd34ad7a3435dcaf82b02e50c023c349de
Jesus_David_Villota_Arteaga <jesus.villotaa@campusucc.edu.co>
2026-09-21 08:38:23 -0500

    feat: setup react router

---
+++ frontend/src/pages/PlantDetail.tsx
@@ -0,0 +1,9 @@
+const PlantDetail = () => {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold">Plant Detail</h1>
+    </div>
+  );
+};
+
+export default PlantDetail;
